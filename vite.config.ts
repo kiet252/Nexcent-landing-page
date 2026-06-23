e864edcb81d48b8f367074a186015fd41da53612
kieta252 <caok104@gmail.com>
2026-06-23 13:33:09 +0700

    Complete navbar layout structure and design rules

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Nexcent-landing-page/', // 👈 Add this line matching your repository name!
 })
