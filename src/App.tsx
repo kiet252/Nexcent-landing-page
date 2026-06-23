7692e04c7f9531c568d8f43478b29bf0e13ea637
kieta252 <caok104@gmail.com>
2026-06-23 12:43:21 +0700

    Initial commit: clean Vite + React starter template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      {/* Your new Nexcent components will go here! */}
+      <h1>Nexcent Project Workspace</h1>
+    </div>
+  )
+}
+
+export default App
